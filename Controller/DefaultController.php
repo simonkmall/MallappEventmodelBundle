85c7b9ebeec9ad5e58f508e9af3659a45cea7726
Simon Mall <simonkmall@gmail.com>
2016-10-20 23:06:30 +0200

    Created 3 TemporalExpressions

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,10 +4,15 @@ namespace Mallapp\EventmodelBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Mallapp\EventmodelBundle\Model\DaySequence;
+
 class DefaultController extends Controller
 {
     public function indexAction()
     {
+        
+        $sequence = new DaySequence(\DateTime::createFromFormat('Y-m-d', '2016-02-15'), \DateTime::createFromFormat('Y-m-d', '2016-02-20'));
+        
         return $this->render('MallappEventmodelBundle:Default:index.html.twig');
     }
 }
